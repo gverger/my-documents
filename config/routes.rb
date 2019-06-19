977f3bcf2eaed7a7244ffc215288e6bc8aa65c6e
Guillaume Verger <verger.guillaume@gmail.com>
2019-06-19 18:01:34 +0200

    Allow to read the pdf files

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'documents#index'
 
   resources :documents, only: %i[index edit update new create]
+
+  mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
 end
