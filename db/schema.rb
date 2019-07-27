5afe6c3148807879e2504614ca932fa07da9e1f8
Guillaume Verger <verger.guillaume@gmail.com>
2019-07-27 12:33:35 +0200

    Database is now postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_06_19_154146) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -42,8 +45,8 @@ ActiveRecord::Schema.define(version: 2019_06_19_154146) do
   end
 
   create_table "documents_tags", id: false, force: :cascade do |t|
-    t.integer "document_id", null: false
-    t.integer "tag_id", null: false
+    t.bigint "document_id", null: false
+    t.bigint "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["document_id"], name: "index_documents_tags_on_document_id"
