c00da3de8debc01ee495d13eb643d3681de90e66
Guillaume Verger <verger.guillaume@gmail.com>
2020-09-21 20:45:41 +0200

    Use session to identify cable actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+  before_action :set_action_cable_identifier
 
   def not_authenticated
     redirect_to new_login_path
+  end
+
+  private
+
+  def set_action_cable_identifier
+    cookies.encrypted[:session_id] = session.id.to_s
   end
 end
